3342c32c5b1d34fc37cb868be5bdc4e0ecdb7295
ina0311 <numberg0311@gmail.com>
2024-01-30 00:54:00 +0900

    store model

---
+++ api/src/models/store.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model, type Sequelize, type InferCreationAttributes, type InferAttributes, type CreationOptional} from 'sequelize'
+import { Models as ModelsType } from './index'
+
+export default class Store extends Model<InferAttributes<Store>, InferCreationAttributes<Store>> {
+  public readonly id!: CreationOptional<number>
+  public readonly name!: string
+  public readonly placeId!: string
+  public readonly siteUrl?: string
+  public readonly createdAt!: CreationOptional<Date>
+  public readonly updatedAt!: CreationOptional<Date>
+
+  public static associate(models: typeof ModelsType) {
+    this.hasMany(models.CoffeeBean, {as: 'coffeeBeans', foreignKey: 'storeId', sourceKey: 'id'})
+  }
+
+  public static initialize(sequelize: Sequelize) {
+    this.init({
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      placeId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      siteUrl: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    },
+    {
+      tableName: 'stores',
+      modelName: 'Store',
+      sequelize
+    })
+    return this
+  }
+}
