9ab0f67e6cab632421f5cd8f2e9cd38673a052db
ina0311 <numberg0311@gmail.com>
2024-02-05 16:07:35 +0900

    flavor select

---
+++ front/src/components/coffeeBean/inputFlavorSelect.vue
@@ -0,0 +1,122 @@
+<template>
+  <Field :name="props.name" :rules="props.rules" validateOnInput @blur="hideDropDown" v-model="fieldValue">
+    <label :for="props.name" class="label">{{ props.label }}</label>
+    <Combobox.Root
+      :multiple="true"
+    >
+      <Combobox.Anchor>
+        <Combobox.Input
+          :name="props.name"
+          :value="props.value"
+          :placeholder="props.placeholder"
+          :type="props.type"
+        >
+        </Combobox.Input>
+        <Combobox.Trigger>
+          <span class="icon is-small is-right">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </Combobox.Trigger>
+      </Combobox.Anchor>
+
+      <Combobox.Content>
+        <Combobox.Viewport>
+          <Combobox.Empty />
+          <Combobox.Group>
+            <Combobox.Item v-for="option in parentFlavorOptions" :key="option.id" :value="option.id" @select.prevent>
+              <Combobox.ItemIndicator>
+              </Combobox.ItemIndicator>
+              <FlavorItem
+                :flavor="option"
+                :flavorList="flavorOptions"
+              />
+            </Combobox.Item>
+          </Combobox.Group>
+        </Combobox.Viewport>
+      </Combobox.Content>
+    </Combobox.Root>
+  </Field>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+import { Field } from 'vee-validate'
+import { FLAVOR_LIST, FLAVOR_TYPE } from '@/utils/constants'
+import { Combobox } from 'radix-vue/namespaced'
+import FlavorItem from './FlavorItem.vue'
+
+const flavorOptions = ref<(FLAVOR_TYPE & { checked: boolean })[]>(FLAVOR_LIST.map(option => ({ ...option, checked: false })))
+const parentFlavorOptions = flavorOptions.value.filter(option => option.parentId === null)
+const fieldValue = ref<string>("")
+
+const isShowDropDown = ref<boolean>(false)
+const hideDropDown = () => {
+  setTimeout(() => {
+    isShowDropDown.value = false
+  }, 100)
+}
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: Array as () => string[],
+    default: []
+  },
+  placeholder: {
+    type: String,
+    default: ""
+  },
+  type: {
+    type: String,
+    default: "text"
+  },
+  rules: {
+    type: String,
+    default: ""
+  },
+})
+
+</script>
+<style scoped lang="stylus">
+
+.overlay
+  position: fixed
+  top: 0
+  left: 0
+  right: 0
+  bottom: 0
+  background-color: rgba(0, 0, 0, 0)
+  z-index: 1
+
+.form-dropdown
+  position: absolute
+  width 50%
+  max-height 20rem
+  overflow-y scroll
+  background-color #fff
+  border-radius 8px
+  box-shadow 0 2px 4px rgba(0, 0, 0, 0.1)
+  z-index 10
+
+  .menu-wrap
+    border-bottom 1px solid #ddd
+
+  .menu
+    padding 0.5rem 1rem
+
+    &.is-active
+      display block
+
+    label
+      margin-left 0.5rem
+      cursor pointer
+      &:hover
+        color #3273dc
+</style>
